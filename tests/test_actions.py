aeccc108145c24c8ecd208843cd962a396fc8bb2
GISRedeDev <dm@gisrede.com>
2024-04-28 14:21:42 +0100

    Check another test

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,6 +1,6 @@
 def test_github_actions():
-    print("Testing Github Actions_______________________________________________________________________________________________")
     assert True
+
 
 def test_failing_github_action():
     assert False
