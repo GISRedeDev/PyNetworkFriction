118d6c9f5ec5b2550964f2a85064c94c7bc14557
GISRedeDev <dm@gisrede.com>
2024-04-30 14:58:37 +0100

    Making a start

---
+++ net_friction/data_preparation.py
@@ -0,0 +1,23 @@
+
+# - ROADS ---------------------------------------------------------------------
+# Read shapefile or geopackage and subset by classification
+
+
+# Fix topology and save file or return gdf
+
+# Make the graph objects
+
+# - ROADS ---------------------------------------------------------------------
+
+# - BOUNDARIES ---------------------------------------------------------------------
+# Read shapefile or geopackage and subset by administrative level
+
+# calculate the centroid based on pop or geometric centroid
+
+# ------ ACLED ---------------------------------------------------------------------
+# Access api and get the data for the dates required
+
+# Save the data to a csv
+
+# Convert to point dataset
+
