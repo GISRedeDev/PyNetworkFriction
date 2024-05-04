c13d16c110ac2aa98f4f11c5228e6d62125d6402
GISRedeDev <dm@gisrede.com>
2024-05-04 09:27:10 +0100

    Get roads data subset and project

--- net_friction/data_preparation.py
+++ net_friction/data_preparation.py
@@ -1,5 +1,32 @@
+from pathlib import Path
+
+import geopandas as gpd
+
 # - ROADS ---------------------------------------------------------------------
 # Read shapefile or geopackage and subset by classification
+def get_roads_data(
+        file_path: Path | str,
+        layer: str | None = None,
+        crs: int | None = None,
+        subset_fields: list | None = None,
+        subset_categories: list | None = None) -> gpd.GeoDataFrame:
+    file_path = Path(file_path)
+    if file_path.suffix == '.shp':
+        roads = gpd.read_file(file_path, layer=layer)
+    elif file_path.suffix == '.gpkg':
+        roads = gpd.read_file(file_path, layer=layer)
+    else:
+        raise ValueError('File type not supported. Please use shapefile or geopackage.')
+    
+    if crs:
+        roads = roads.to_crs(crs)
+    if subset_fields:
+        subset_fields.append("geometry")
+        roads = roads[subset_fields]
+    if subset_categories:
+        roads = roads[roads["fclass"].isin(subset_categories)]
+    return roads
+    
 
 
 # Fix topology and save file or return gdf
---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from pathlib import Path
+
+from net_friction.data_preparation import get_roads_data
+
+BASE_DATA = Path(__file__).resolve().parent.joinpath('test_data')
+
+@pytest.fixture
+def get_test_roads_data_subset_and_projected():
+    yield get_roads_data(
+        BASE_DATA.joinpath('ROADS_TEST.shp'),
+        crs=6383,
+        subset_fields=["osm_id", "fclass"],
+        subset_categories=["motorway", "trunk", "primary", "secondary", "tertiary"])
